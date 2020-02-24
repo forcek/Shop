d8d3bc4b0344eecaffe36578cc5716ce796e75b8
Abuasim Khan <kabuasim@gmail.com>
2020-02-24 18:20:24 +0530

    Interface Theme Completed

---
+++ shop/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -0,0 +1,8 @@
+
+<div class="container-fluid footer bg-dark ">
+	<div class="text">
+		<p class="m-0 text-center text-white">Copyright &copy; Your
+			Website 2020</p>
+	</div>
+</div>
+<!-- /.container -->
---
+++ shop/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,22 @@
+$(function(){
+	
+	switch(menu){
+	
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	case 'Services':
+		$('#services').addClass('active');
+		break; 
+	default :
+		$('#home').addClass('active');
+		break;
+			
+	
+	}
+	
+}
+);
